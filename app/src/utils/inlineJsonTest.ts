a85e19db0b1c3a61cc4fa915dca676609cf57a8c
Eugenio <eugenio.barrero@gmail.com>
2024-09-17 12:10:03 +0200

    remove testing

--- app/src/utils/inlineJsonTest.ts
+++
@@ -1,9 +0,0 @@
-import test from 'node:test';
-import assert from 'node:assert';
-import { salesforceJsontoInlineJson } from './inlineJson';
-import json  from '../Mocks/resp.json' assert { type: 'json'};
-
-test('inlineJson', () => {
-  const result = salesforceJsontoInlineJson(json);
-  assert(result === 'Hello, World!');
-});
