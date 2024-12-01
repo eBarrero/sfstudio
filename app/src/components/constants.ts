23eb2a1e12123bc91930fed467fe3e59c6d1d45d
Eugenio <eugenio.barrero@gmail.com>
2024-12-01 12:37:40 +0100

    FIX DEPLY

--- app/src/components/constants.ts
+++ app/src/components/constants.ts
@@ -16,8 +16,8 @@ export enum IconCharacter  {
     WHERE = '&#128269;',   // 🔍  U+1F50D
     EXE = "🗲", //'&#128498;', U+1F5F2
     ARROW1 = "⮘",
-    CHECK = '✔️' // &#10003 U+2713
-    CROSS = '&#10007;',    // ❌ U+274C
+    CHECK = '✔️', // &#10003 U+2713
+    CROSS = '❌'  // &#10007 U+274C
 
   }
 
